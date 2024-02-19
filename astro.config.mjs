72449177cd11f7b35d8123ba1ca1ffc32c0480dd
Alex-zReeZ <alexbeck220@gmail.com>
2024-02-19 13:27:06 +0100

    add : installed Reactjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import icon from "astro-icon";
-
 import sitemap from "@astrojs/sitemap";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon(), sitemap()]
+  integrations: [icon(), sitemap(), react()]
 });
