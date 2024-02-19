840403bfebcd11183f4ac08617cb8c275010b8ca
Alex-zReeZ <alexbeck220@gmail.com>
2024-02-19 13:50:59 +0100

    fix : repared nav.js file

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,7 +1,7 @@
-const navLinks = document.querySelector("[data-navlink]")
+const navLinks = document.querySelectorAll("[data-navlink]");
 
 navLinks.forEach((link) => {
     if (link.getAttribute("href") === window.location.pathname) {
-        link.setAttribute("aria-current", "page")
+        link.setAttribute("aria-current", "page");
     }
-})
+});
